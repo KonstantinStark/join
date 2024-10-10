408bda2eb66f8c85e3e94e8c213a23af98b5309d
Konstantin Stark <konstantin.stark.dev@gmail.com>
2024-10-10 13:50:03 +0200

    add remoteStorage

---
+++ js/remoteStorage.js
@@ -0,0 +1,65 @@
+function onLoadFunc() {
+    console.log("test");
+    putData("/name", { "Apfel": "Essig" });
+}
+
+const BASE_URL = "https://remotestorage-128cc-default-rtdb.europe-west1.firebasedatabase.app/";
+
+async function putData(path = "", data = {}) {
+    let response = await fetch(BASE_URL + path + ".json", {
+        method: "PUT",
+        header: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data)
+    });
+    return responseToJson = await response.json();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+async function loadData(path = "") {
+    let response = await fetch(BASE_URL + path + ".json");
+    return responseToJson = await response.json();
+}
+
+async function postData(path = "", data = {}) {
+    let response = await fetch(BASE_URL + path + ".json", {
+        method: "POST",
+        header: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data)
+    });
+    return responseToJson = await response.json();
+}
+
+async function deleteData(path = "") {
+    let response = await fetch(BASE_URL + path + ".json", {
+        method: "DELETE",
+    });
+    return responseToJson = await response.json();
+}
+
+
+
+
+
+
+
+
+
+
+
+
