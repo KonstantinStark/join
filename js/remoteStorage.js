094aa44cc3879f06a3bd6ad60a8cc82149c92b9c
Konstantin Stark <konstantin.stark.dev@gmail.com>
2024-10-11 09:01:21 +0200

    check remoteStorage

--- js/remoteStorage.js
+++ js/remoteStorage.js
@@ -1,33 +1,9 @@
 function onLoadFunc() {
     console.log("test");
-    putData("/name", { "Apfel": "Essig" });
+    postData("/name", { "Apfel": "Essig" });
 }
 
 const BASE_URL = "https://remotestorage-128cc-default-rtdb.europe-west1.firebasedatabase.app/";
-
-async function putData(path = "", data = {}) {
-    let response = await fetch(BASE_URL + path + ".json", {
-        method: "PUT",
-        header: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(data)
-    });
-    return responseToJson = await response.json();
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 async function loadData(path = "") {
     let response = await fetch(BASE_URL + path + ".json");
