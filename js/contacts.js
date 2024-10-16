bd923579d13e4e511c859bc6729b2a64d953242b
Konstantin Stark <konstantin.stark.dev@gmail.com>
2024-10-16 09:09:16 +0200

    some fixes

--- js/contacts.js
+++ js/contacts.js
@@ -1,9 +1,7 @@
 function toggleOverlay() {
     let overlay = document.getElementById('overlay');
-
     if (overlay.classList.contains('d-none')) {
-        overlay.classList.remove('d-none'); 
-        
+        overlay.classList.remove('d-none');         
         setTimeout(function() {
             overlay.classList.add('show');
         }, 10);
