914e38eba8e0ee16fa3f1be7d65295e452804a8d
tmlbwski <dmdmlbwski@gmail.com>
2024-10-18 11:30:01 +0200

    update

--- firebase.js
+++ firebase.js
@@ -1,6 +1,13 @@
 let users = [];
 const FIREBASE_URL = "https://remotestorage-128cc-default-rtdb.europe-west1.firebasedatabase.app/";
 
+
+
+
+
+
+// ab hier nach contacts.js
+
 function init() {
     loadUsers();
 }
