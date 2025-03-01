62ac4f3b05bc1ddc9a37f8e2d35d4aeb8f082515
matt-e-dev <dmdmlbwski@gmail.com>
2025-03-01 18:35:42 +0100

    moving category for phone finished

--- js/board-templates.js
+++ js/board-templates.js
@@ -1,39 +1,50 @@
 function generateTaskCardTemplate(task, categoryClass, userAvatars, progressData) {
+
     return `
 
-          <p onclick="renderCategoryMenu('${task.id}')"><-></p>
-          
-          <!-- Category Menu will be rendered here -->
-          <div id="categoryMenu-${task.id}" class="d-none"></div>
+    <div class="task-card-template-wrapper">
 
         <div id="task-${task.id}" class="single-task-card" draggable="true" onclick="taskCardsOverlay('${task.id}')"
             ondragstart="startDragging(event, '${task.id}')" ondrop="handleDrop(event, '${task.boardCategory}')" ondragover="allowDrop(event)">
             
-            <div class="task-cards-header-wrapper">
-                <p class="task-category ${categoryClass}">${task.category}</p>
-            </div>
-            
-            <h3>${task.title}</h3>
-            <p>${task.description}</p>
+                <div class="task-cards-header-wrapper">
+                    <p class="task-category ${categoryClass}">${task.category}</p>
+                </div>
+                
+                <h3>${task.title}</h3>
+                <p>${task.description}</p>
 
-            ${progressData ? `
-                <div class="subtask-progress">
-                    <div class="progress-bar">
-                        <div class="progress-fill" style="width: ${progressData.progressPercentage}%;"></div>
+                ${progressData ? `
+                    <div class="subtask-progress">
+                        <div class="progress-bar">
+                            <div class="progress-fill" style="width: ${progressData.progressPercentage}%;"></div>
+                        </div>
+                        <span>${progressData.completedSubtasks}/${progressData.totalSubtasks} Subtasks</span>
+                    </div>` : ""}
+
+                <div class="assigned-users-prio-button-wrapper">
+                    <div class="assigned-users">
+                        ${userAvatars || ""}
                     </div>
-                    <span>${progressData.completedSubtasks}/${progressData.totalSubtasks} Subtasks</span>
-                </div>` : ""}
 
-            <div class="assigned-users-prio-button-wrapper">
-                <div class="assigned-users">
-                    ${userAvatars || ""}
-                </div>
-
-                <div class="prio-button-board">
-                    <p>${task.prioButton ? getPrioSVG(task.prioButton) : getPrioSVG('medium')}</p>
+                    <div class="prio-button-board">
+                        <p>${task.prioButton ? getPrioSVG(task.prioButton) : getPrioSVG('medium')}</p>
+                    </div>
                 </div>
             </div>
-        </div>`;
+
+                <div class="mobile-category-menu">
+
+                    <p onclick="renderCategoryMenu('${task.id}')"><-></p>
+                    
+                    <!-- Category Menu will be rendered here -->
+                    <div id="categoryMenu-${task.id}" class="d-none rendered-category-menu"></div>
+                
+            </div> 
+        </div>
+        
+        `;
+        
 }
 
 
